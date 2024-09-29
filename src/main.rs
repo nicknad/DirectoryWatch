ba0c0130511832006fd67480b438c8e230abd748
nicknad <nadolskinick@gmail.com>
2024-09-29 23:13:19 +0200

    fixed time now

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 use std::path::Path;
 use std::io;
 use std::process::Command;
-use std::time::SystemTime;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 pub fn main() {
     let args : Vec<String> = env::args().collect();
@@ -14,8 +14,15 @@ pub fn main() {
     let directory = Path::new(&args[1]);
     // compare time to file metadata and 
     // if changes occur update time and execute commands from args
-    let mut point_in_time = SytemTime::now();
-    traverse_dir(&directory);
+    let mut point_in_time = SystemTime::now();
+    print_time(&point_in_time);
+
+
+    let result = traverse_dir(&directory);
+
+    if !result.is_ok() {
+        panic!("traverse failed")
+    }
 
     if args.len() == 2 {
         panic!("No commands given");
@@ -35,6 +42,15 @@ pub fn main() {
                 .expect("failed to execute process")
         };
     }
+    point_in_time = SystemTime::now();
+    print_time(&point_in_time);
+}
+
+fn print_time(time: &SystemTime) {
+    let since_the_epoch = time.duration_since(UNIX_EPOCH).expect("Time went backwards");
+    let in_ms = since_the_epoch.as_secs() * 1000 +
+            since_the_epoch.subsec_nanos() as u64 / 1_000_000;
+    println!("{:?}", in_ms);
 }
 
 fn traverse_dir(dir: &Path) -> io::Result<()>{
