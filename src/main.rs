11b2c3d3e6f0d437fb3c7feed4fedeb7f5fd62d2
nicknad <nadolskinick@gmail.com>
2024-10-03 21:45:04 +0200

    some improvment in the printout of the executed commands

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use anyhow::{Result, anyhow};
+use anyhow::{anyhow, Result};
 use std::env;
 use std::fs;
 use std::path::Path;
 use std::process::Command;
-use std::time::{SystemTime, Duration};
 use std::thread;
+use std::time::{Duration, SystemTime};
 
 // Expects following input via arguments
 // executable name (rust default)
 // path to directory
 // cmds1..n
 pub fn main() -> Result<()> {
-    let args : Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     let number_of_args = args.len();
 
     if number_of_args == 1 {
@@ -21,29 +21,35 @@ pub fn main() -> Result<()> {
     if number_of_args == 2 {
         return Err(anyhow!("No commands given"));
     }
-    
+
     let directory = Path::new(&args[1]);
     let mut point_in_time = SystemTime::now();
 
     loop {
         thread::sleep(Duration::from_millis(500));
-        let files_changed = traverse_dir(&directory, &point_in_time)?; 
+        let files_changed = traverse_dir(&directory, &point_in_time)?;
 
         if !files_changed {
             continue;
         }
-        
+
         println!("file changed");
         point_in_time = SystemTime::now();
         for cmd in &args[2..] {
-            if cfg!(target_os = "windows") {
-                Command::new(cmd)
-                    .output()?;
+            let parts: Vec<&str> = cmd.split(" ").collect();
 
+            if parts.len() == 1 {
+                let output = Command::new(cmd)
+                    .output()
+                    .expect("Failed to execute command!");
+                println!("{:?}", String::from_utf8_lossy(&output.stdout));
             } else {
-                Command::new(cmd)
-                    .output()?;
-            };
+                let output = Command::new(parts[0])
+                    .args(&parts[1..])
+                    .output()
+                    .expect("Failed to execute command!");
+                println!("{:?}", String::from_utf8_lossy(&output.stdout));
+            }
         }
     }
 }
@@ -58,12 +64,10 @@ fn traverse_dir(dir: &Path, point_in_time: &SystemTime) -> Result<bool> {
             if has_changed_file {
                 return Ok(true);
             }
-
         } else if path.is_file() {
-
             let meta = path.metadata()?;
             let mod_time = meta.modified()?;
-            if  *point_in_time < mod_time {
+            if *point_in_time < mod_time {
                 return Ok(true);
             }
         }
